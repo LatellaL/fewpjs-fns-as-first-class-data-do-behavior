20b719448e49dac16f84843141abbc7c423978f9
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-24 10:08:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 /* Given Code, don't edit */
-
 document.querySelector("button").addEventListener("click", handleClick);
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
-
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeStr) {
+  const time = parseInt(timeStr, 10)
+  if (time < 12) return "Good Morning"
+  if (time > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
